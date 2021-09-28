2bbed463bf24845850c275e1f86bff15beb86781
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-09-28 22:35:40 +0700

    02-05 static optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/dist/client/router'
+import React, { useEffect } from 'react'
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
+	const router = useRouter()
+
+	console.log('About query: ', router.query)
+
 	return <div>About Page</div>
+}
+
+export async function getServerSideProps() {
+	return {
+		props: {}, // will be passed to the page component as props
+	}
 }
