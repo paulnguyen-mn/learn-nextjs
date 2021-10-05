b998b43ad0d616cd3bc63050d119625e5a0c6666
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-05 14:51:06 +0700

    02-06 ssg coding
    - fetch data to build post list page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,39 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import * as React from 'react'
 
-export interface PostListPageProps {}
+export interface PostListPageProps {
+	posts: any[]
+}
 
-export default function PostListPage(props: PostListPageProps) {
-	return <div>Post List Page</div>
+export default function PostListPage({ posts }: PostListPageProps) {
+	console.log('posts', posts)
+
+	return (
+		<div>
+			<h1>Post List Page</h1>
+
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+	context: GetStaticPropsContext
+) => {
+	// server-side
+	// build-time
+	console.log('static props')
+	const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1')
+	const data = await response.json()
+	console.log(data)
+
+	return {
+		props: {
+			posts: data.data.map((x: any) => ({ id: x.id, title: x.title })),
+		},
+	}
 }
