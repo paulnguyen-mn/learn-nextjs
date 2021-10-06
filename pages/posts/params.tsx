4305612f7211235fe43ab1380c2a5e41b1b0bde8
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-10-06 16:54:24 +0700

    02-09 ssr demo

--- pages/posts/params.tsx
+++ pages/posts/params.tsx
@@ -14,3 +14,12 @@ export default function ParamsPage(props: ParamsPageProps) {
 		</div>
 	)
 }
+
+export async function getServerSideProps() {
+	// fake slow query
+	await new Promise((resolve) => setTimeout(resolve, 3000))
+
+	return {
+		props: {},
+	}
+}
