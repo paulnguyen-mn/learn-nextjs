bc4052e86d66639b2fbd722610f3fe44fb7673f9
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-11-10 21:28:44 +0700

    04-04 play with emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,15 +11,11 @@ import '../styles/globals.css'
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({
-	Component,
-	pageProps,
-	emotionCache = clientSideEmotionCache,
-}: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	const Layout = Component.Layout ?? EmptyLayout
 
 	return (
-		<CacheProvider value={emotionCache}>
+		<CacheProvider value={clientSideEmotionCache}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 
