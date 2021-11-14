818e011c97321ea05abbf912cb6a9704f9b61d04
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-11-14 16:09:11 +0700

    04-05 font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,6 +58,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
 	ctx.renderPage = () =>
 		originalRenderPage({
+			// eslint-disable-next-line react/display-name
 			enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
 		})
 
