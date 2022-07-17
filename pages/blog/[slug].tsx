5fdd6e8db14afba7e301824824803f2b9338a62b
Paul <paulnguyen.mn@gmail.com>
2022-07-17 10:40:59 +0700

    05-08 md to html

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,13 @@
 import { Post } from '@/models'
 import { getPostList } from '@/utils/posts'
+import { Container } from '@mui/material'
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify/lib'
+import remarkParse from 'remark-parse/lib'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
 
 export interface BlogPageProps {
 	post: Post
@@ -10,14 +17,14 @@ export default function PostDetailPage({ post }: BlogPageProps) {
 	if (!post) return null
 
 	return (
-		<div>
+		<Container>
 			<h1>Post Detail Page</h1>
 
 			<p>{post.title}</p>
 			<p>{post.author?.name}</p>
 			<p>{post.description}</p>
-			<p>{post.mdContent}</p>
-		</div>
+			<div dangerouslySetInnerHTML={{ __html: post.htmlContent || '' }}></div>
+		</Container>
 	)
 }
 
@@ -39,6 +46,16 @@ export const getStaticProps: GetStaticProps<BlogPageProps> = async (
 
 	const post = postList.find((x) => x.slug === slug)
 	if (!post) return { notFound: true }
+
+	// parse md to html
+	const file = await unified()
+		.use(remarkParse)
+		.use(remarkRehype)
+		.use(rehypeDocument, { title: 'Blog details page' })
+		.use(rehypeFormat)
+		.use(rehypeStringify)
+		.process(post.mdContent || '')
+	post.htmlContent = file.toString()
 
 	return {
 		props: {
