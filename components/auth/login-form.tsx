aff520fe75fa4cf8f59fa2ce75b8a1cde271e1ef
Paul <paulnguyen.mn@gmail.com>
2022-10-08 14:50:12 +0700

    06-04 input field

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,13 +1,16 @@
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { Button, IconButton, InputAdornment } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { InputField } from '../form'
 
 export function LoginForm() {
+	const [showPassword, setShowPassword] = useState(false)
 	const { control, handleSubmit } = useForm({
 		defaultValues: {
-			username: 1,
-			password: 5,
+			username: '',
+			password: '',
 		},
 	})
 
@@ -18,7 +21,28 @@ export function LoginForm() {
 	return (
 		<Box component="form" onSubmit={handleSubmit(handleLoginSubmit)}>
 			<InputField name="username" control={control} />
-			<InputField name="password" control={control} />
+			<InputField
+				type={showPassword ? 'text' : 'password'}
+				name="password"
+				control={control}
+				InputProps={{
+					endAdornment: (
+						<InputAdornment position="end">
+							<IconButton
+								aria-label="toggle password visibility"
+								onClick={() => setShowPassword((x) => !x)}
+								edge="end"
+							>
+								{showPassword ? <VisibilityOff /> : <Visibility />}
+							</IconButton>
+						</InputAdornment>
+					),
+				}}
+			/>
+
+			<Button type="submit" variant="contained">
+				Login
+			</Button>
 		</Box>
 	)
 }
--- components/form/input-field.tsx
+++ components/form/input-field.tsx
@@ -1,14 +1,23 @@
+import { TextField, TextFieldProps } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 import { Control, useController } from 'react-hook-form'
 
-export interface InputFieldProps {
+export type InputFieldProps = TextFieldProps & {
 	name: string
-	label?: string
 	control: Control<any>
 }
 
-export function InputField({ name, label, control }: InputFieldProps) {
+export function InputField({
+	name,
+	label,
+	control,
+	onChange: externalOnChange,
+	onBlur: externalOnBlur,
+	ref: externalRef,
+	value: externalValue,
+	...rest
+}: InputFieldProps) {
 	const {
 		field: { onChange, onBlur, value, ref },
 		fieldState: { error },
@@ -19,8 +28,16 @@ export function InputField({ name, label, control }: InputFieldProps) {
 
 	// render whatever you want: MUI, Ant Design, Bootstrap, Custom UI
 	return (
-		<Box onClick={() => onChange(value + 1)} ref={ref}>
-			{name}: {value}
-		</Box>
+		<TextField
+			fullWidth
+			size="small"
+			margin="normal"
+			name={name}
+			value={value}
+			onChange={onChange}
+			onBlur={onBlur}
+			inputRef={ref}
+			{...rest}
+		/>
 	)
 }
